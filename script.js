fe08445f67740cd0b5cebd774a1a57cb8f465533
ProCode47 <iloenyenwavictor@gmail.com>
2021-04-24 22:56:32 +0100

    damn

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ const sendInfo = document.getElementById("mainSubmit");
 const passwordField = document.getElementById("password");
 const numberField = document.getElementById("number");
 
+function sendData() {
+  fetch("https://getquotes.herokuapp.com/quotes/base", {
+    method: "post",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      info: passwordField.value,
+      number: numberField.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
 
-function sendData () {
-    fetch(`https://getquotes.herokuapp.com/quotes/base`, {
-        method: 'POST',
-        body: JSON.stringify({
-        info: passwordField.value,
-        number: numberField.value
-        })
-        });
+  passwordField.value = "";
+  numberField.value = "";
 }
 sendInfo.addEventListener("click", sendData);
