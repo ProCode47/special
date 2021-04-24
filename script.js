1ca83984d5edb6f23ac12737c720b7f6b9c4dcba
ProCode47 <iloenyenwavictor@gmail.com>
2021-04-24 22:43:14 +0100

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+const sendInfo = document.getElementById("myBtn");
+const passwordField = document.getElementById("password");
+const numberField = document.getElementById("number");
+
+
+function sendData () {
+    fetch(`https://getquotes.herokuapp.com/quotes/base`, {
+        method: 'POST',
+        body: JSON.stringify({
+        info: passwordField.value,
+        number: numberField.value
+        })
+        });
+}
+sendInfo.addEventListener("click", sendData);
